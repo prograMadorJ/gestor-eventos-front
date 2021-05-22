3698b683185f1215d256adf52b8c63885c1d9941
jorge <jorge.fernando.brito@gmail.com>
2021-05-21 21:09:01 -0300

    criado o component Main

---
+++ src/components/layout/Main.jsx
@@ -0,0 +1,16 @@
+import {Card} from "react-bootstrap";
+import './Main.scss'
+
+function Main() {
+  return (
+    <div className="main-app">
+      <Card className="card-panel">
+        <Card.Body>
+          Main
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
+
+export default Main;
