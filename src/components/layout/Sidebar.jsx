21ab71680cad7e5f8c7d33bd7500a9bac85428d1
jorge <jorge.fernando.brito@gmail.com>
2021-05-22 16:27:32 -0300

    ajustes na sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -2,6 +2,7 @@ import {Card, Nav} from "react-bootstrap";
 import { BsTable } from 'react-icons/bs';
 import { BsFillPersonFill } from 'react-icons/bs';
 import './Sidebar.scss'
+import {Link} from "react-router-dom";
 
 function Sidebar() {
   return (
@@ -9,22 +10,22 @@ function Sidebar() {
       <Card className="card-panel">
         <Card.Body>
           <Nav defaultActiveKey="/home" className="flex-column">
-            <Nav.Link href="/home">
+            <Link to="/home" className="nav-link">
               <div className="d-flex align-items-center">
                 <BsTable className="mr-2"/>
                 <span>
                 Eventos
                 </span>
               </div>
-            </Nav.Link>
-            <Nav.Link eventKey="link-1">
+            </Link>
+            <Link to="/usuarios"  className="nav-link">
               <div className="d-flex align-items-center">
                 <BsFillPersonFill className="mr-2"/>
                 <span>
                 Usuários
                 </span>
               </div>
-            </Nav.Link>
+            </Link>
           </Nav>
         </Card.Body>
       </Card>
