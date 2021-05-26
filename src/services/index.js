7e6bc682f4197360f481c4cae2455feb0a29e185
jorge <jorge.fernando.brito@gmail.com>
2021-05-26 11:37:51 -0300

    ajustes na url da api para acessar o app no heroku

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://localhost:8080/api`
+  baseURL: process.env.NODE_ENV === 'production'
+    ? 'https://desafio-nkey-gestor-eventos.herokuapp.com/api'
+    : `http://localhost:8080/api`
 });
 
 export const jwt = accessToken => ({headers: {
